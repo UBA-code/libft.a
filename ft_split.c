87a8f18afb80192b174fbbdc744b897008b7cdba
UBA-code <belhachmiyassine4@gmail.com>
2022-08-22 15:24:26 +0100

    ft_split work before changes

---
+++ ft_split.c
@@ -0,0 +1,77 @@
+#include "libft.h"
+#include "stdio.h"
+
+int char_count(const char *s, int c)
+{
+  int i;
+  int size;
+
+  size = 0;
+  i = 0;
+  while (s[i])
+  {
+    if (s[i] == c)
+      size++;
+    i++;
+  }
+  return (size + 1);
+}
+
+int *alloc_copy(char **final, char *s, int *i, int *j, int *x)
+{
+  ++*x;
+  final[*x] = malloc(sizeof(char) * (*i - *j));
+  if (final[*x] == NULL)
+    return (NULL);
+  ft_strlcpy(final[*x], (char *)s + *j, *i - *j + 1);
+}
+
+char **ft_split(char const *s, char c)
+{
+  int i;
+  int j;
+  int x;
+  char **final_tab;
+
+  final_tab = malloc(sizeof(char*) * char_count((char *)s, c));
+  if (final_tab == NULL)
+    return (NULL);
+  i = -1;
+  j = 0;
+  x = -1;
+  while (s[++i])
+  {
+    if (s[i] == c)
+    {
+      if (alloc_copy(final_tab, (char *)s, &i, &j, &x) == NULL)
+        return (NULL);
+      j = ++i;
+    }
+  }
+  if (alloc_copy(final_tab, (char *)s, &i, &j, &x) == NULL)
+    return (NULL);
+  final_tab[x + 1] = '\0';
+  return (final_tab);
+}
+/*
+int main()
+{
+  char str[] = "Hello,Guys,Welcome,home,everybody,slm,youssef,yassine,omar,zbi,tbonmok";
+  char **s = ft_split(str, ',');
+  int i = 0;
+  /*
+    printf("%s,", s[0]);
+    printf("%s,", s[1]);
+    printf("%s,", s[2]);
+    printf("%s,", s[3]);
+    printf("%s,", s[4]);
+    printf("%s,", s[5]);
+    printf("%s", s[6]);
+    */
+  while (s[i])
+  {
+    printf("%s, ", s[i]);
+    i++;
+  }
+  return 0;
+}*/
