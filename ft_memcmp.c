4623be80feea40e51d1d89501b41f85536c9bf3a
UBA-code <belhachmiyassine4@gmail.com>
2022-08-25 01:46:39 +0100

    add some functions add ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,4 +1,19 @@
+#include "libft.h"
+
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    
+    const char *str1;
+    const char *str2;
+
+    str1 = s1;
+    str2 = s2;
+    if (n == 0)
+        return (0);
+    while (*str1 == *str2 && n > 0)
+    {
+        *str1++;
+        *str2++;
+        n--;
+    }
+    return (*str1 - *str2);
 }
